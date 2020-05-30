d3526f196a3a565d3b6dd1bfcf8896aeee946915
Nicolas Mariscotti <nicolas.mariscotti93@gmail.com>
2020-05-30 19:11:54 -0300

    creating team section

--- src/components/Team.js
+++ src/components/Team.js
@@ -5,7 +5,9 @@ import './Team.css';
 const Team = () => {
     return (
         <div className="team__container" id="teamSection">
-
+            <h1 className="section-title">
+                Team Members
+            </h1>
         </div>
     )
 }
